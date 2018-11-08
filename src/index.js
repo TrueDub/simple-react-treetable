9fe3327560b78d6fa1ca44a27dbe397f48915b74
Jim Gallagher <gallagherji@dnb.com>
2018-11-08 16:50:34 +0000

    expand working correctly

--- src/index.js
+++ src/index.js
@@ -7,6 +7,51 @@ let dataFields = ["name", "dataType", "example", "description"];
 let tableData = [
     {
         data: {
+            name: "name0",
+            dataType: "string0",
+            example: "ex0",
+            description: "desc0"
+        },
+        children: [
+            {
+                data: {
+                    name: "name0-0",
+                    dataType: "string0-0",
+                    example: "ex0-0",
+                    description: "desc0-0"
+                },
+                children: []
+            }, {
+                data: {
+                    name: "name0-1",
+                    dataType: "string0-1",
+                    example: "ex0-1",
+                    description: "desc0-1"
+                },
+                children: []
+            }, {
+                data: {
+                    name: "name0-2",
+                    dataType: "string0-2",
+                    example: "ex0-2",
+                    description: "desc0-2"
+                },
+                children: [
+                    {
+                        data: {
+                            name: "name0-2-1",
+                            dataType: "string0-2-1",
+                            example: "ex0-2-1",
+                            description: "desc0-2-1"
+                        },
+                        children: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        data: {
             name: "name1",
             dataType: "string1",
             example: "ex1",
@@ -20,15 +65,6 @@ let tableData = [
             dataType: "string2",
             example: "ex2",
             description: "desc2"
-        },
-        children: []
-    },
-    {
-        data: {
-            name: "name3",
-            dataType: "string3",
-            example: "ex3",
-            description: "desc3"
         },
         children: []
     }
--- src/lib/TreeTable.jsx
+++ src/lib/TreeTable.jsx
@@ -1,25 +1,81 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAngleRight, faAngleDown} from '@fortawesome/free-solid-svg-icons'
 
 import "bootstrap/dist/css/bootstrap.css";
+import "./TreeTable.css";
 
 class TreeTable extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.rowExpandOrCollapse = this.rowExpandOrCollapse.bind(this);
+        this.state = {enhancedTableData: this.generateStateTableData(this.props.tableData)};
+    }
+
+    generateStateTableData(tree, n = 1) {
+        return (function recurse(children, parent = 0) {
+            return children.map(node => {
+                let rowID = n++;
+                return Object.assign({}, node, {
+                    rowID: rowID,
+                    parentRowID: parent,
+                    visible: parent === 0 ? true : false,
+                    expanded: false,
+                    children: recurse(node.children, rowID)
+                })
+            });
+        })(tree);
+    }
+
+    rowExpandOrCollapse = (selectedRowID) => {
+        console.log(selectedRowID);
+        let newTree = (function recurse(children) {
+            return children.map(node => {
+                console.log(node);
+                let setExpanded = node.rowID === selectedRowID ? true : node.expanded;
+                let setVisible = node.parentRowID === selectedRowID ? true : node.visible;
+                return Object.assign({}, node, {
+                    visible: setVisible,
+                    expanded: setExpanded,
+                    children: recurse(node.children)
+                })
+            });
+        })(this.state.enhancedTableData);
+        this.setState({enhancedTableData: newTree});
+    };
+
     generateTableBody(dataFields, tableData) {
         let tableBody = [];
-        tableBody.push(tableData.map((dataRow, index) => {
+        tableData.forEach((dataRow, index) => {
                 let rowData = this.processDataRow(dataFields, dataRow);
-                let key = 'r' + index;
-                return <tr key={key}>{rowData}</tr>
+                let key = dataRow.parentRowID + "-" + dataRow.rowID;
+                let rowClass = dataRow.visible ? "shown" : "hidden";
+                tableBody.push(<tr className={rowClass} key={key}>{rowData}</tr>);
+                if (dataRow.children) {
+                    tableBody.push(...this.generateTableBody(dataFields, dataRow.children));
+                }
             }
-        ));
+        );
         return tableBody;
     }
 
     processDataRow(dataFields, dataRow) {
         let rowBody = [];
+        if (dataRow.children.length > 0) {
+            let iconCell = <FontAwesomeIcon icon={faAngleRight}
+                                            onClick={this.rowExpandOrCollapse.bind(this, dataRow.rowID)}/>;
+            if (dataRow.expanded) {
+                iconCell = <FontAwesomeIcon icon={faAngleDown}
+                                            onClick={this.rowExpandOrCollapse.bind(this, dataRow.rowID)}/>;
+            }
+            rowBody.push(<td key="f0">{iconCell}</td>);
+        } else {
+            rowBody.push(<td key="f0"></td>);
+        }
         rowBody.push(dataFields.map((dataField, index) => {
-                let key = 'r' + index;
+                let key = dataRow.parentRowID + "-" + dataRow.rowID + '-' + index;
                 return (<td key={key}>{dataRow.data[dataField]}</td>)
             }
         ));
@@ -27,11 +83,12 @@ class TreeTable extends React.Component {
     }
 
     render() {
-        let headingRows = [];
+        let headingRows = [<th key="r0"></th>];
         headingRows.push(this.props.columnHeadings.map((heading) =>
             <th key={heading}>{heading}</th>
         ));
-        let tableBody = this.generateTableBody(this.props.dataFields, this.props.tableData);
+        console.log(this.state.enhancedTableData);
+        let tableBody = this.generateTableBody(this.props.dataFields, this.state.enhancedTableData);
         return (
             <table className="table">
                 <thead>
